4ad1c3f7ecfd6f7240670019c2376f5af9e55cf9
Matt Light <matt.light@lightdatasys.com>
2015-10-29 23:50:10 -0700

    Use YoPdo for DatabaseService logic
    DatabaseService logic was abstracted to a new project,
    YoPdo, so that the logic can be used without dependencies
    such as Silex.

--- src/Lstr/Silex/Database/DatabaseService.php
+++ src/Lstr/Silex/Database/DatabaseService.php
@@ -10,8 +10,7 @@
 
 namespace Lstr\Silex\Database;
 
-use Pdo;
-
+use Lstr\YoPdo\Factory as YoPdoFactory;
 use Silex\Application;
 
 class DatabaseService
@@ -19,7 +18,7 @@ class DatabaseService
     private $app;
     private $config;
 
-    private $pdo;
+    private $yo_pdo;
 
     public function __construct(Application $app, array $config)
     {
@@ -29,97 +28,43 @@ class DatabaseService
 
     public function getPdo()
     {
-        if (null !== $this->pdo) {
-            return $this->pdo;
-        }
-
-        $dsn      = $this->config['dsn'];
-        $username = isset($this->config['username']) ? $this->config['username'] : null;
-        $password = isset($this->config['password']) ? $this->config['password'] : null;
-        $options  = isset($this->config['driver_options']) ? $this->config['driver_options'] : null;
-
-        $this->pdo = new PDO($dsn, $username, $password, $options);
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-        return $this->pdo;
+        return $this->getYoPdo()->getPdo();
     }
 
     public function query($sql, array $params = array())
     {
-        return $this->queryWithOptions($sql, $params);
+        return $this->getYoPdo()->query($sql, $params);
     }
 
     public function queryMultiple($sql, array $params = array())
     {
-        // turn on query emulation so multiple queries can be ran
-        $options = array(
-            PDO::ATTR_EMULATE_PREPARES => true,
-        );
-
-        return $this->queryWithOptions($sql, $params, $options);
+        return $this->getYoPdo()->queryMultiple($sql, $params);
     }
 
     public function getLastInsertId($sequence_table = null)
     {
-        return $this->getPdo()->lastInsertId($sequence_table);
+        return $this->getYoPdo()->getLastInsertId($sequence_table);
     }
 
     public function insert($tablename, array $values)
     {
-        $columns      = array();
-        $placeholders = array();
-        foreach ($values as $column => $value) {
-            $columns[]      = $column;
-            $placeholders[] = ":{$column}";
-        }
-
-        $column_sql      = implode(",\n", $columns);
-        $placeholder_sql = implode(",\n", $placeholders);
-
-        $this->query(
-            "
-                INSERT INTO {$tablename}
-                (
-                    {$column_sql}
-                )
-                VALUES
-                (
-                    {$placeholder_sql}
-                )
-            ",
-            $values
-        );
+        return $this->getYoPdo()->insert($tablename, $values);
     }
 
     public function update($tablename, array $set_cols, $where_sql, array $values)
     {
-        $sets = array();
-        foreach ($set_cols as $column => $placeholder) {
-            if (is_numeric($column)) {
-                $column = $placeholder;
-            }
-            $sets[] = "{$column} = :{$placeholder}";
-        }
-
-        $set_sql = implode(",\n", $sets);
-
-        $this->query(
-            "
-                UPDATE {$tablename}
-                SET {$set_sql}
-                WHERE {$where_sql}
-            ",
-            $values
-        );
+        return $this->getYoPdo()->update($tablename, $set_cols, $where_sql, $values);
     }
 
-    private function queryWithOptions($sql, array $params = array(), array $options = array())
+    private function getYoPdo()
     {
-        $pdo   = $this->getPdo();
-        $query = $pdo->prepare($sql, $options);
-        $query->execute($params);
+        if ($this->yo_pdo) {
+            return $this->yo_pdo;
+        }
 
-        return $query;
+        $factory = new YoPdoFactory();
+        $this->yo_pdo = $factory->createFromConfig($this->config);
+
+        return $this->yo_pdo;
     }
 }
